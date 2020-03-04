2329c11476baf4259483beefb4c66b5f95ba99eb
Shidan Hemmat <sxh2751@med.miami.edu>
2020-03-04 15:52:05 -0500

    updated tab js

--- src/js/vendor/pilltabs.js
+++ src/js/vendor/pilltabs.js
@@ -6,9 +6,9 @@ $('#myTab a').click(function (e) {
 })
 
 
-$('#myTab a[href="#pills-catalog"]').tab('show'); // Select tab by name
-$('#myTab a[href="#pills-pubmed"]').tab('show');
-$('#myTab a[href="#pills-uptodate"]').tab('show');
-$('#myTab a[href="#pills-dynamed"]').tab('show');
+$('#myTab a[href="#catalog"]').tab('show'); // Select tab by name
+$('#myTab a[href="#pubmed"]').tab('show');
+$('#myTab a[href="#uptodate"]').tab('show');
+$('#myTab a[href="#dynamed"]').tab('show');
 
 
